90f2737580532a2c604ae685a5fd5194f311719d
Rerey <144551736+Rerey155-del@users.noreply.github.com>
2026-03-09 00:58:41 +0700

    oke

--- src/main/java/com/example/guru_api/config/SecurityConfig.java
+++ src/main/java/com/example/guru_api/config/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+            // .cors(cors -> {})
             .cors(cors -> {})
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
